3727e9144a4622bf26e71bdcb5628c5d8227beb0
AlinaB <bernardezalina@gmail.com>
2024-01-08 10:43:38 +0100

    clase

--- networkModule.mjs
+++ networkModule.mjs
@@ -1,13 +1,16 @@
 import os from 'os'
 
 export function getNetworkData() {
-    const networkArray = Object.entries(os.networkInterfaces())
-    for(let i = 0; i < networkArray.length; i++) {
-        let interfaz = networkArray[i][0]
-        let family = networkArray[i][1][1].family
-        let address = networkArray[i][1][1].address
-        let internal = networkArray[i][1][1].internal
-        console.log(`Interfaz: ${interfaz} , Familia: ${family} , Dirección: ${address}, Interno: ${internal}`)
-    }
+    const networks = os.networkInterfaces()
+    const redes = {}
+
+    Object.keys(networks).forEach(net => {
+        redes[net] = networks[net].map(interFace => ({
+            familia: interFace.family,
+            direccion: interFace.address,
+            interno: interFace.internal
+        }))
+    })
+    return redes
 }
 
